3434c64760cf81f88a5aaeab5558e9c76b5fc5cd
vitush <vit.ush@mail.ru>
2023-08-10 12:33:08 +0300

    add form_for method

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,4 +4,11 @@ require_relative 'hexlet_code/version'
 
 module HexletCode
   autoload :Tag, 'hexlet_code/tag'
+
+  def self.form_for(_model, attributes = {})
+    action = attributes.fetch(:url, '#')
+    method = attributes.fetch(:method, 'post')
+
+    Tag.build 'form', action:, method:
+  end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -3,7 +3,29 @@
 require 'test_helper'
 
 class TestHexletCode < Minitest::Test
+  User = Struct.new(:name, :job, keyword_init: true)
+
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
+  end
+
+  def test_empty_form_with_default_attributes_is_generated
+    user = User.new name: 'Rob'
+    expected = '<form action="#" method="post"></form>'
+    actual = HexletCode.form_for user do |_f|
+      'nothing'
+    end
+
+    assert_equal expected, actual
+  end
+
+  def test_empty_form_is_generated
+    user = User.new name: 'Rob'
+    expected = '<form action="/users" method="post"></form>'
+    actual = HexletCode.form_for user, url: '/users' do |_f|
+      'nothing'
+    end
+
+    assert_equal expected, actual
   end
 end
