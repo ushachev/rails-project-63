5250d632fcbf342a5b3b871016a2443c31f5477e
vitush <vit.ush@mail.ru>
2023-08-13 12:20:33 +0300

    fix dependencies for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,8 @@ $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'hexlet_code'
 
 require 'minitest/autorun'
+require 'bundler/setup'
+Bundler.require
 
 def load_fixture(filename)
   File.read(File.dirname(__FILE__) + "/fixtures/#{filename}").strip
